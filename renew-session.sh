313e97641b62d06759262322c81eaaedb81eebef
Peng <tpeng@HIUSA129273.attlocal.net>
2021-01-08 17:21:36 -0600

    remove hardcode comment

--- renew-session.sh
+++ renew-session.sh
@@ -2,7 +2,6 @@
 profile=$1
 account=$2
 role=$3
-#new_credential=$(aws --profile saml-hcs-sandbox-Admin sts assume-role --role-arn arn:aws:iam::630527842429:role/Admin --role-session-name terraform-tiger --query "Credentials.{aws_access_key_id:AccessKeyId,aws_secret_access_key:SecretAccessKey,aws_session_token:SessionToken}" --output json | grep aws_ |sed 's/^ *"//; s/": "/ = /; s/",*$//')
 while true
 do
 	new_credential=$(aws --profile $profile sts assume-role --role-arn arn:aws:iam::$account:role/$role --role-session-name renewed-$(date "+%Y-%m-%dT%H.%M.%S") --query "Credentials.{aws_access_key_id:AccessKeyId,aws_secret_access_key:SecretAccessKey,aws_session_token:SessionToken}" --output json | grep aws_ |sed 's/^ *"//; s/": "/ = /; s/",*$//')
