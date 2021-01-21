d55b31f04e7a48770979b58597963c8c0953ebac
Peng <tpeng@HIUSA66105.ad.harman.com>
2021-01-21 11:31:03 -0600

    only pass profile parameter

--- renew-session.sh
+++ renew-session.sh
@@ -1,10 +1,9 @@
 #!/usr/local/bin/bash
 profile=$1
-account=$2
-role=$3
+read -r account arn userid <<< $(aws --profile saml-hcs-fca-integration-Admin sts get-caller-identity  --query "[Account,Arn]" --output text |sed 's|sts|iam|; s|assumed-||; s|/\([^/]*\)$| \1|')
 while true
 do
-	new_credential=$(aws --profile $profile sts assume-role --role-arn arn:aws:iam::$account:role/$role --role-session-name renewed-$(date "+%Y-%m-%dT%H.%M.%S") --query "Credentials.{aws_access_key_id:AccessKeyId,aws_secret_access_key:SecretAccessKey,aws_session_token:SessionToken}" --output json | grep aws_ |sed 's/^ *"//; s/": "/ = /; s/",*$//')
+  new_credential=$(aws --profile $profile sts assume-role --role-arn $arn --role-session-name $userid --duration-seconds 3600 --query "Credentials.{aws_access_key_id:AccessKeyId,aws_secret_access_key:SecretAccessKey,aws_session_token:SessionToken}" --output json | grep aws_ |sed 's/^ *"//; s/": "/ = /; s/",*$//')
   sed -i.bak "/\[$profile\]/,+5d"  ~/.aws/credentials
   echo "[$profile]" >> ~/.aws/credentials
   echo "output = json" >> ~/.aws/credentials
